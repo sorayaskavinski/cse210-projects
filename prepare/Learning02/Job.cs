3b1f8b242bfaa32307e770a2be51d87b837f9c8d
sorayaskavinski <soraya1br@gmail.com>
2024-09-21 16:04:04 -0700

    corrected

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,12 +2,12 @@ using System;
 
 public class Job
 {
-    public string _JobTitle;
-    public string _JobCompany;
+    public string _jobTitle;
+    public string _company;
     public int _startYear;
     public int _endYear;
 
-    punlic void Display()
+    public void Display()
     {
         Console.WriteLine($"{_JobTitle} ({_JobCompany}) {_startYear}-{_endYear}");
     }
