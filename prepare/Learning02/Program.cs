ae4945ca7180bce0c5d5c3d8c570bd2cbb1fa6cb
sorayaskavinski <soraya1br@gmail.com>
2024-09-21 17:02:24 -0700

    new

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engeneer";
+        job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
@@ -17,10 +17,11 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Alisson Rose";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
+        
 
         myResume.Display();
     }
