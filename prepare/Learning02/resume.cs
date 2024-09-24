2b393b3d7cdad12db3c0e6b9c6fd0ebcbb337e30
sorayaskavinski <soraya1br@gmail.com>
2024-09-23 17:23:08 -0700

    ..

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -2,8 +2,9 @@ using System;
 
 public class Resume
 {
+    public string _name;
     public List<Job> _jobs = new List<Job>();
-
+    
     public void Display()
     {
         Console.WriteLine($" Name: {_name}");
